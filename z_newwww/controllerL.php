1e5b63537ac2ad41f34cbceff12a22d1861a6648
Your Name <you@example.com>
2024-10-18 19:30:04 +0800

    .

--- z_newwww/controllerL.php
+++ z_newwww/controllerL.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ . '/../config/databaseconfig.php'; // leave dir alone
-require_once __DIR__ . '/../lib/Database.php'; 
+require_once __DIR__ . '/../config/configLR.php'; // leave dir alone
+require_once __DIR__ . '/../lib/dbconnL.php'; 
 
 class AuthController
 {
